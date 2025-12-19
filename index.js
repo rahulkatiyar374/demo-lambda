2973ac6b6f2d1661ee84c131f62e5d4dcae784ee
Rahul Katiyar <rahulkatiyar374@gmail.com>
2025-12-19 11:31:29 +0530

    index.js add

--- index.js
+++ index.js
@@ -6,5 +6,15 @@ const app = express()
 app.use('/api/', (req,res)=>{
     res.send("hello lambda")
 })
-
+app.use('/api/getUser', (req,res)=>{
+    res.status(200).json({
+        message:"success",
+        data:{
+            name:"Rahul Katiyar",
+            contact:"8174821563",
+            email:"rkatiyar@gmail.com",
+            address:"Hinjwadi phase 2 Pune"
+        }
+    })
+})
 module.exports.handler = serverless(app)
